691b2093383c28af573c1c531e0fa2d2aba2ab2b
Thais Camacho <thaiscamachoo@gmail.com>
2019-05-18 08:34:10 -0300

    uniform LUT without error

--- pdi/trab1/library/algorithms.py
+++ pdi/trab1/library/algorithms.py
@@ -2,27 +2,26 @@ import numpy as np
 import cv2
 
 def splitCube(n):
-    return 4,3,2
+    return 5,5,5
 
 def createLUT(n):
     colors = (np.linspace(0, 255, n, dtype=np.int))
-    dist = int(np.ceil(colors[1]/2))
     LUT = np.zeros(256)
 
-    for i in range(1,n):
-        k = colors[i] 
-        LUT[k-dist:k+dist+1] = k
+    for i in range(0, 256):
+        a = abs(colors - i)
+        m = a.min()
+        k, = np.where(a == m)
+        LUT[i] = colors[k[0]]
 
-    LUT[colors[n-1]-dist:colors[n-1]+1] = colors[n-1]
     return LUT
 
 def uniform(img_in, n):
-    a,b,c = splitCube(n)
+    x,y,z = splitCube(n)
     r,g,b = cv2.split(img_in)
-    r[:] = (createLUT(4))[r[:]]
-    g[:] = (createLUT(3))[g[:]]
-    b[:] = (createLUT(2))[b[:]]
-    
+    r[:] = (createLUT(x))[r[:]]
+    g[:] = (createLUT(y))[g[:]]
+    b[:] = (createLUT(z))[b[:]]
     return cv2.merge([r,g,b])
 
 def medianCut(img_in, n):
