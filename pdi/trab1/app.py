77d904475c786be2b33725a5c0e27ade81a48c9d
Thais Camacho <thaiscamachoo@gmail.com>
2019-05-18 09:04:27 -0300

    vai ser em um script só, pq essa ***** não funciona

--- pdi/trab1/app.py
+++ pdi/trab1/app.py
@@ -6,8 +6,8 @@ Created on Fri May 17 17:36:24 2019
 @author: thais
 """
 
-from library import images
-from library import algorithms
+import numpy as np
+import cv2
 
 imgName = 'images/strawberries_fullcolor.tif'
 n = 50
@@ -15,18 +15,62 @@ mode = 1
 # 1: Uniform
 # 2: Median Cut
 
+def readSG(name): # read shades of gray image
+    return cv2.imread(name, 0)
+
+def readRGB(name):
+    bgr_img = cv2.imread(name)
+    b,g,r = cv2.split(bgr_img)
+    return cv2.merge([r,g,b])
+
+def show(label, img):
+    cv2.imshow(label, img)
+    
+def showRGB(label, img):
+    r,g,b = cv2.split(img)
+    cv2.imshow(label, cv2.merge([b,g,r]))
+    
+def splitCube(n):
+    return 4,3,2
+
+def createLUT(n):
+    colors = np.linspace(0, 255, n, dtype=np.int)
+    LUT = np.zeros(256)
+
+    for i in range(0, 256):
+        a = abs(colors - i)
+        m = a.min()
+        k, = np.where(a == m)
+        LUT[i] = colors[k[0]]
+
+    return LUT
+
+def uniform(img_in, n):
+    x,y,z = splitCube(n)
+    r,g,b = cv2.split(img_in)
+
+    r[:] = (createLUT(x))[r[:]]
+    g[:] = (createLUT(y))[g[:]]
+    b[:] = (createLUT(z))[b[:]]
+
+    return cv2.merge([r,g,b])
+
+def medianCut(img_in, n):
+    return img_in
+
 def main():
-    img_in = images.readRGB(imgName)
+    img_in = readRGB(imgName)
+    img = img_in.copy()
     
     if(mode == 1):
-        img_out = uniform(img_in, n);
+        img_out = uniform(img, n);
     else:
-        img_out = medianCut(img_in, n);
+        img_out = medianCut(img, n);
     
-    images.showRGB('image in', img_in)
-    images.showRGB('image out', img_out)
+    showRGB('image in', img_in)
+    showRGB('image out', img_out)
     
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 main()
